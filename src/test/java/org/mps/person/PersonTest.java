6d129f3fc839f64219cf506f9432ec9554dfa316
ignamarga1 <0619967319@uma.es>
2023-02-15 00:28:32 +0100

    Update PersonTest.java

--- src/test/java/org/mps/person/PersonTest.java
+++ src/test/java/org/mps/person/PersonTest.java
@@ -1,7 +1,37 @@
 package org.mps.person;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PersonTest {
+/**
+ * Class for testing Person.java
+ *
+ * @author Ignacio Martínez Gallardo
+ */
 
+/*
+* Test cases
+* */
+class PersonTest {
+    Person person;
+    List<Person> persons;
+
+    @BeforeEach
+    void setup() {
+        person = new Person("Ignacio", 21, "Male");
+
+        persons = new ArrayList<>();
+    }
+
+    @AfterEach
+    void shutdown() {
+        for(Person p : persons) {
+            p = null;
+        }
+    }
 }
