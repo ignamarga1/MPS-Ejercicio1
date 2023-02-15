199ad156880d7c31654bb38a170c905a7c4e6ebe
ignamarga1 <0619967319@uma.es>
2023-02-15 01:45:17 +0100

    Update PersonTest.java

--- src/test/java/org/mps/person/PersonTest.java
+++ src/test/java/org/mps/person/PersonTest.java
@@ -49,4 +49,67 @@ class PersonTest {
         double expectedValue = 34;
         assertEquals(expectedValue,obtainedValue[0]);
     }
+
+    @Test
+    void averageAgeOf1FemaleShouldReturnHerAge() {
+        persons.add(person2);
+        double[] obtainedValue = person.averageAgePerGender(persons);
+        double expectedValue = 87;
+        assertEquals(expectedValue,obtainedValue[1]);
+    }
+
+    @Test
+    void averageAgeOf1MaleAnd1FemaleShouldReturnTheirAge() {
+        persons.add(person3);
+        persons.add(person4);
+
+        double[] obtainedValue = person.averageAgePerGender(persons);
+        double[] expectedValue = new double[2];
+        expectedValue[0] = 12;
+        expectedValue[1] = 56;
+
+        assertArrayEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    void averageAgeOfAllMalesShouldReturnTheMeanOfTheirAges() {
+        persons.add(person1);
+        persons.add(person4);
+        persons.add(person6);
+
+        double[] obtainedValue = person.averageAgePerGender(persons);
+        double expectedValue = 30.666666666666668;
+
+        assertEquals(expectedValue, obtainedValue[0]);
+    }
+
+    @Test
+    void averageAgeOfAllFemalesShouldReturnTheMeanOfTheirAges() {
+        persons.add(person2);
+        persons.add(person3);
+        persons.add(person5);
+
+        double[] obtainedValue = person.averageAgePerGender(persons);
+        double expectedValue = 63.333333333333336;
+
+        assertEquals(expectedValue, obtainedValue[1]);
+    }
+
+    @Test
+    void averageAgeOfAllPersonsShouldReturnTheMeanOfTheirAges() {
+        persons.add(person1);
+        persons.add(person2);
+        persons.add(person3);
+        persons.add(person4);
+        persons.add(person5);
+        persons.add(person6);
+
+        double[] obtainedValue = person.averageAgePerGender(persons);
+        double[] expectedValue = new double[2];
+        expectedValue[0] = 30.666666666666668;
+        expectedValue[1] = 63.333333333333336;
+
+        assertArrayEquals(expectedValue, obtainedValue);
+    }
+
 }
